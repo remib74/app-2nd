cfd59ca9f19de566bb723e69c37d901a23f3a2eb
remi bouffort <33649152+remib74@users.noreply.github.com>
2019-11-26 17:23:38 +0100

    Update App.js
    upload the code from local dir

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+var randNum=Math.random(this.state.value);
+var num=Math.floor((this.state.value*randNum));
+    this.setState({value: num.toString(16)});
+    event.preventDefault();
+  }
+
+render() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>2nd app</h1>
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <label for="txtNum">Entrez un nombre ici... (max 13 caractères)</label><br/>
+            <textarea value={this.state.value} className="txtNum" maxlength="13"  onChange={this.handleChange}/>
+            <br/>
+          <input type="submit" value="Envoyer" />
+          </form>
+
+        </div>
       </header>
     </div>
   );
 }
+}
+
 
 export default App;
+
